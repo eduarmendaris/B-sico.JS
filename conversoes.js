b9623e074bf2a072dc5c9a56684da3c9d5754450
Armendaris-Edu <108244150+Armendaris-Edu@users.noreply.github.com>
2022-08-28 08:10:48 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,24 @@
+
+
+// Tipos de dados 
+// booleanos
+
+// conversão implicita
+
+// null undefined  -> esses dois valores são tratados como iguais
+// "0" == 0 -> a string é convertida em um número antes da comparação 
+// 0 == false -> o booleano é convertido em um número ante da comparação 
+//"0" == false -> os dois operadores são convertidos em número antes da comparação
+
+// Realizada pelo operador (==) para determinar se dois valores devem ser considerados iguais e também descreve
+//o operador de igualdade restrito (===),que não faz conversões ao testar a igualdade.
+
+
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero + numeroString);
+
+
+
+
---
+++ truthy-falsy.js
@@ -0,0 +1,33 @@
+
+// boolean 
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// 0 => false
+// 1 => true
+
+/*
+console.log( 0 == false)
+console.log("" == false)
+console.log( 1 == true)
+*/
+
+//NULL normalmente utilizado para indicar a ausencia de um valor
+
+//UNDEFINED voce pode pensar em usar undefined para representar a ausencia 
+//de valor em um nivel de sistema..
+
+
+/*
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+*/
+
+ var numero = 3;
+ let texto = "Eduardo";
+ console.log(typeof numero);
+ console.log(typeof texto);
